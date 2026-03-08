880d5da99330650c7904d68deae8897b9f32c9ec
Jannik Ellendt <jannik.ellendt@web.de>
2026-03-08 23:51:50 +0100

    chore: init setup

---
+++ branch-name-lint.js
@@ -0,0 +1,23 @@
+const branch = require('child_process')
+  .execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+
+const pattern = /^(feat|fix|chore|refactor|docs|test|style|perf)\/[a-z0-9-]+$/;
+
+if (!pattern.test(branch)) {
+  console.error(`
+❌ Invalid branch name: ${branch}
+
+Allowed format:
+
+type/name
+
+Examples:
+feat/login-page
+fix/auth-token
+chore/update-deps
+`);
+
+  process.exit(1);
+}
